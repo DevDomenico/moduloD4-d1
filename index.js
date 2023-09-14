afae8c1ffe6b308943dc522c1e0d05bcd567c81d
Domenico <135279072+DevDomenico@users.noreply.github.com>
2023-09-14 12:45:50 +0200

    esercizio

--- index.js
+++ index.js
@@ -9,7 +9,27 @@ console.log(somma50(25, 25))
 
 // 2. Crea una funzione che rimuova il carattere ad una specifica posizione da una stringa. Passa la stringa e la posizione come parametri e ritorna la stringa modificata.
 
-// da vedere....
+function rimuoviCarattere(stringa, posizione) {
+  if (posizione < 0 || posizione >= stringa.length) {
+    // Se la posizione è fuori dai limiti della stringa, restituisci la stringa originale.
+    return stringa;
+  } else {
+    // Utilizza il metodo `slice` per rimuovere il carattere in posizione specifica.
+    return stringa.slice(0, posizione) + stringa.slice(posizione + 1);
+  }
+}
+
+// Esempio 
+const stringaOriginale = "Hello, World!";
+const nuovaStringa = rimuoviCarattere(stringaOriginale, 5); // Rimuove la virgola
+console.log(nuovaStringa); // Output: "Hello World!"
+
+// In questa funzione, verifico se la posizione specificata è all'interno dei limiti della stringa.
+// Se la posizione è valida (cioè maggiore o uguale a zero e minore della lunghezza della stringa),
+// utilizziamo il metodo `slice` per rimuovere il carattere in quella posizione dalla stringa originale.
+// Successivamente, restituiamo la stringa modificata.
+// Se la posizione è fuori dai limiti della stringa (negativa o maggiore o uguale alla lunghezza della stringa),
+// semplicemente restituiamo la stringa originale senza apportare modifiche.
 
 // 3. Crea una funzione che controlli se due numeri siano compresi tra 40 e 60 o tra 70 e 100. Ritorna `true` se rispecchiano queste condizioni, altrimenti ritorna `false`.
 
@@ -41,7 +61,21 @@ console.log(controllaCitta("New York"));      // New York
 // 5. Crea una funzione che calcoli e ritorni la somma di tutti gli elementi di un array.
 // L’array deve essere passato come parametro.
 
-// da vedere.....
+function sommaArray(array) {
+  let somma = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    somma += array[i];
+  }
+
+  return somma;
+}
+
+//utilizzo:
+const arrayNumeri = [1, 2, 3, 4, 5];
+const risultato = sommaArray(arrayNumeri);
+console.log(risultato); // Output: 15
+
 
 // 6. Crea una funzione che controlli che un array NON contenga i numeri 1 o 3. Se NON li contiene, ritorna `true`, altrimenti ritorna `false`.
 
@@ -67,7 +101,19 @@ console.log(controllaArray(array1)); // true (non contiene 1 o 3)
 // Angolo retto: 90° ⇒ ritorna “retto”
 // Angolo piatto: 180° ⇒ ritorna “piatto”
 
-//da vedere...
+const angoli = (deg) => {
+  if (deg < 90) return "acuto";
+  if (deg === 90) return "retto";
+  if (deg > 90 && deg < 180) return "ottuso";
+  if (deg === 180) return "piatto";
+  
+  return "gradi non validi"; // Gestione dei gradi non validi
+}
+
+//utilizzo:
+console.log(angoli(45));  // "acuto"
+
+
 
 // 8. Crea una funzione che crei un acronimo a partire da una frase. Es. “Fabbrica Italiana Automobili Torino” deve ritornare “FIAT”.
 
